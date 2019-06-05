09d6539fc86594418b6fc3793befcfad5571063b
DustinGaudet <dustin@dustingaudet.com>
2019-06-05 16:19:21 -0400

    Remove internal state from App

--- src/components/App.js
+++ src/components/App.js
@@ -7,43 +7,10 @@ import Modal from './Modal.js'
 
 class App extends Component {
 
-  state = {
-    user: {
-      name: "John Smith",
-      email: "john.smith@gmail.com",
-      profilePicture: "https://example.com/img-url.jpg"
-    },
-    tasks: {
-      "2019-06-01-a8b02624": {
-        name: "Cool Project 1"
-      },
-      "2019-06-02-c8b94323": {
-        name: "Cool Project 2"
-      },
-      "2019-06-04-e8b89425": {
-        name: "Cool Project 3"
-      },
-    },
-    taskOrder: {
-      "root": [
-        "2019-06-01-a8b02624",
-        "2019-06-02-c8b94323",
-        "2019-06-04-e8b89425",
-      ]
-    },
-    activeTaskId: "2019-06-01-a8b02624"
-  }
-
-  tasksInOrder = (taskOrderArr, tasksStateObj) => taskOrderArr.map(x => ({...tasksStateObj[x], id: x}))
-
   render() {
-    const {tasks, taskOrder} = this.state
     return (
       <div className="app">
-        <HomeInterface 
-          taskOrder={taskOrder} 
-          projects={this.tasksInOrder(taskOrder.root, tasks)} 
-          />
+        <HomeInterface />
         <TasksInterface />
         <TaskDetailInterface />
         <Modal />
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Provider} from 'react-redux'
 import renderer from 'react-test-renderer';
 import App from './App'
+import configureStore from '../store'
+
+const store = configureStore()
 
 describe('Basic App tests', () => {
     it('renders without crashing', () => {
-        renderer.create(<App />)
+        renderer.create(<Provider store={store}><App /></Provider>)
     })
     
 });
