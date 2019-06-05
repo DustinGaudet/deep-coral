faed9d2f21300941727b2f87c6f7062c45767031
DustinGaudet <dustin@dustingaudet.com>
2019-06-05 16:19:06 -0400

    Connect HomeInterface to the redux store

--- src/components/HomeInterface.js
+++ src/components/HomeInterface.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import './HomeInterface.scss'
 import ProjectItem from './ProjectItem'
 
-const HomeInterface = ({projects}) => (
-  <div className="home-interface">
-    <div className="user-modal-open-btn">User Modal open btn here</div>
-    <div className="projects-list">
-      <ul>
-        {projects && projects.map(x => <ProjectItem key={'project' + x.id} project={x} />)}
-      </ul>
+export const HomeInterface = ({taskOrder = {"root": []}, tasks = {}}) => {
+  
+  const projects = (taskOrder, tasks) => taskOrder.root.map(x => (
+    <ProjectItem key={x} project={tasks[x]} />
+  ))
+  return (
+    <div className="home-interface">
+      <div className="user-modal-open-btn">User Modal open btn here</div>
+      <div className="projects-list">
+        <ul>
+          {taskOrder.root.length && projects(taskOrder, tasks)}
+        </ul>
+      </div>
+      <div className="new-project-modal-open-btn">New Project modal open btn here</div>
     </div>
-    <div className="new-project-modal-open-btn">New Project modal open btn here</div>
-  </div>
-)
+  )
+}
 
-export default HomeInterface
+const ConnectedHomeInterface = connect((state)=> {
+  return {
+    taskOrder: state.taskOrder,
+    tasks: state.tasks,
+  }
+})(HomeInterface)
+
+export default ConnectedHomeInterface
--- src/components/HomeInterface.test.js
+++ src/components/HomeInterface.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer';
-import HomeInterface from './HomeInterface'
+import {HomeInterface} from './HomeInterface'
 
 describe('Basic HomeInterface tests', () => {
     it('renders without crashing', () => {
